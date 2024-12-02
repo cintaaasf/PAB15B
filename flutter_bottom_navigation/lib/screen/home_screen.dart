282f0fcf6c7fa4d43df8619c1d082812b935c7db
cintaaasf <riadisafina@gmail.com>
2024-12-02 10:24:50 +0700

    Flutter Bottom Navigation

---
+++ flutter_bottom_navigation/lib/screen/home_screen.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Home'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: const Text('Ini adalah halaman home'),
+      ),
+    );
+  }
+}
