9f56fa57d6284a3547892e8e9dd795b59862c513
cintaaasf <riadisafina@gmail.com>
2024-12-02 10:29:28 +0700

    complete all screens code

---
+++ flutter_bottom_navigation/lib/screen/favorite_screen.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class FavoriteScreen extends StatelessWidget {
+  const FavoriteScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Favorite'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: const Text('Ini adalah halaman favorite'),
+      ),
+    );
+  }
+}
---
+++ flutter_bottom_navigation/lib/screen/profile_screen.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class ProfileScreen extends StatelessWidget {
+  const ProfileScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Profile'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: const Text('Ini adalah halaman profile'),
+      ),
+    );
+  }
+}
