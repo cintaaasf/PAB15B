13e9e2cc6c391491cbb87211563ba6023e4436f8
cintaaasf <riadisafina@gmail.com>
2024-12-02 11:00:57 +0700

    finish project

--- flutter_bottom_navigation/lib/main.dart
+++ flutter_bottom_navigation/lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bottom_navigation/screen/favorite_screen.dart';
 import 'package:flutter_bottom_navigation/screen/home_screen.dart';
+import 'package:flutter_bottom_navigation/screen/main_screen.dart';
 
 void main() {
   runApp(const MainApp());
@@ -10,8 +12,6 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: HomeScreen()
-    );
+    return const MaterialApp(home: MainScreen());
   }
 }
